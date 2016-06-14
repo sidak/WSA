2927f1449c7c31ce2271d6ab2ce7d3fdf0105aca
xuedong <shang.xuedong@yahoo.fr>
2016-06-14 18:00:59 +0900

    Add endPoints.m to create a hashmap between scores and reviews

---
+++ scripts/endPoints.m
@@ -0,0 +1,46 @@
+clear all;
+close all;
+
+dirname = 'laptops';
+load(strcat('../mat/', dirname, '_reviewText.mat'));
+load(strcat('../mat/', dirname, '_scores.mat'));
+
+keySet = [1, 2, 3, 4, 5];
+
+dim = size(reviewText);
+len = dim(2);
+
+X1 = [];
+X2 = [];
+X3 = [];
+X4 = [];
+X5 = [];
+
+h = waitbar(0, 'In progress...')
+
+counter = len;
+step = 1/counter;
+
+for i = 1:len
+    waitbar(step*i, h, sprintf('%.2f%%...', step*i*100))
+    n = str2num(cell2mat(scores(i)));
+    switch n
+        case 1
+            X1 = [X1 reviewText(i)];
+        case 2
+            X2 = [X2 reviewText(i)];
+        case 3
+            X3 = [X3 reviewText(i)];
+        case 4
+            X4 = [X4 reviewText(i)];
+        case 5
+            X5 = [X5 reviewText(i)];
+    end
+end
+
+close(h)
+
+valueSet = [{X1}, {X2}, {X3}, {X4}, {X5}];
+scoreMap = containers.Map(keySet, valueSet);
+
+save(strcat('../mat/', dirname, '_scoreMap.mat'), 'scoreMap')
