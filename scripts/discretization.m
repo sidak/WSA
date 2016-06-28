02628dedbba0449231740b5d7bb2d999c7bc21fd
xuedong <shang.xuedong@yahoo.fr>
2016-06-28 19:02:08 +0900

    Changed a parameter in discretization.m

--- scripts/discretization.m
+++ scripts/discretization.m
@@ -16,7 +16,7 @@ step = 1/count;
 for t = 0.025:0.025:4.975
     waitbar(step*i, h, sprintf('%.2f%%...', step*i*100))
     weights = [t 1-t];
-    center = wassersteinBarycenter(ends, M, 10000, 20, false, 1e-8, weights);
+    center = wassersteinBarycenter(ends, M, 10000, 20, false, 1e-4, weights);
     bcenters = [bcenters; center]
 end
 
