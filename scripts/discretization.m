4ee0a4cfbf442918296771eba7f6aefe25823683
xuedong <shang.xuedong@yahoo.fr>
2016-06-28 19:22:16 +0900

    Bug testing for discretization.m.
    Return the number of iterations of each computation of barycenter as
    well.

--- scripts/discretization.m
+++ scripts/discretization.m
@@ -8,19 +8,22 @@ load('../mat/laptops_distMatrix.mat');
 M = M_laptops/median(M_laptops(:));
 bcenters = [];
 ends = [c1 c5];
+counts = [];
 
 h = waitbar(0, 'Calculating the geodesic...');
-count = 41;
-step = 1/count;
+counter = 41;
+step = 1/counter;
 
 for t = 0.025:0.025:4.975
     waitbar(step*i, h, sprintf('%.2f%%...', step*i*100))
     weights = [t 1-t];
-    center = wassersteinBarycenter(ends, M, 10000, 20, false, 1e-4, weights);
-    bcenters = [bcenters; center]
+    [center, count] = wassersteinBarycenter(ends, M, 10000, 20, false, 1e-4, weights);
+    bcenters = [bcenters; center];
+    counts = [counts count];
 end
 
 close(h);
 
 bcenters = [c1; bcenters; c5];
 save('../mat/laptops_geodesic.mat', 'bcenters');
+save('../mat/laptops_counts.mat', 'counts');
