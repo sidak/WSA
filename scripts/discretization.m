6ef3a751845f6aaf88f9249ffd52bab422161343
xuedong <shang.xuedong@yahoo.fr>
2016-06-28 17:17:58 +0900

    Add a discretization.m script.
    This functions computes the geodesic between the minimum and maximum
    histograms by a Eulerian approach.

---
+++ scripts/discretization.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+
+% Loading data
+load('../mat/laptops_barycenter1.mat');
+load('../mat/laptops_barycenter5.mat');
+load('../mat/laptops_distMatrix.mat');
+M = M_laptops/median(M_laptops(:));
+bcenters = [];
+ends = [c1 c5];
+
+h = waitbar(0, 'Calculating the geodesic...');
+count = 41;
+step = 1/count;
+
+for t = 0.1:0.1:4.9
+    waitbar(step*i, h, sprintf('%.2f%%...', step*i*100))
+    weights = [t 1-t];
+    center = wassersteinBarycenter(ends, M, 10000, 20, false, 1e-8, weights);
+    bcenters = [bcenters; center]
+end
+
+close(h);
+
+bcenters = [c1; bcenters; c5];
+save('../mat/laptops_geodesic.mat', 'bcenters');
