b08648e54d231af4119da33ad0de21d8a7909287
xuedong <shang.xuedong@yahoo.fr>
2016-07-01 17:21:13 +0900

    Changed the proportion of training and test data.

--- scripts/partition.m
+++ scripts/partition.m
@@ -1,7 +1,7 @@
 function [train, test] = partition(fname, score)
 load(strcat('../mat/', fname, '_score', int2str(score), '.mat'));
 n = size(m, 1);
-c = cvpartition(n, 'Holdout', 0.2);
+c = cvpartition(n, 'Holdout', 0.25);
 train = m(c.training, :);
 test = m(c.test, :);
 save(strcat('../mat/', fname, '_score', int2str(score), '_train.mat'), 'train');
