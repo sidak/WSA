b55d23280d49d8fd93a35d8544f189b65344a512
xuedong <shang.xuedong@yahoo.fr>
2016-07-01 17:13:21 +0900

    Added a function partition.m
    This function take a filename and a score as arguments, then split the
    corresponding review texts into training and testing dataset.

---
+++ scripts/partition.m
@@ -0,0 +1,9 @@
+function [train, test] = partition(fname, score)
+load(strcat('../mat/', fname, '_score', int2str(score), '.mat'));
+n = size(m, 1);
+c = cvpartition(n, 'Holdout', 0.2);
+train = m(c.training, :);
+test = m(c.test, :);
+save(strcat('../mat/', fname, '_score', int2str(score), '_train.mat'), 'train');
+save(strcat('../mat/', fname, '_score', int2str(score), '_test.mat'), 'test');
+end
