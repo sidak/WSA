11583d269e3cabdbbe308fbd5b8a4f04d2f5c3af
xuedong <shang.xuedong@yahoo.fr>
2016-07-02 20:53:20 +0900

    Simplified some code in discretization.m

--- scripts/discretization.m
+++ scripts/discretization.m
@@ -10,12 +10,7 @@ bcenters = [];
 ends = [c1 c5];
 %counts = [];
 
-h = waitbar(0, 'Calculating the geodesic...');
-counter = 41;
-step = 1/counter;
-
 for t = 0.025:0.025:0.975
-    waitbar(step*i, h, sprintf('%.2f%%...', step*i*100))
     weights = [t 1-t];
     center = wassersteinBarycenter(ends, M, 10000, 20, false, 1e-4, weights);
     center = center';
