bb791febe80ac1bef478ff19f96e26a44c19c7d7
xuedong <shang.xuedong@yahoo.fr>
2016-07-04 18:10:39 +0900

    New tests.
    Removed some frequent words.

--- scripts/preprocessBis.m
+++ scripts/preprocessBis.m
@@ -1,24 +1,24 @@
 clear all;
 close all;
 
-fname = 'laptops';
-
-load(strcat('../mat/', fname, '_wordsRed.mat'));
-
-cd word2vec/;
-[dictRed, vecRed] = readWord2Vec('../../data/word2vec/GoogleNews-vectors-negative300.bin', wordsRed, 1);
-cd ..;
-
-save(strcat('../mat/', fname, '_dictRed.mat'), 'dictRed');
-save(strcat('../mat/', fname, '_vecRed.mat'), 'vecRed');
-
-M_cos = distMatrix(fname, 'cosine');
-save('../mat/laptops_distMatrixCos.mat', 'M_cos');
-M_euc = distMatrix(fname, 'euclidean');
-save('../mat/laptops_distMatrixEuc.mat', 'M_euc');
+% fname = 'laptops';
+% 
+% load(strcat('../mat/', fname, '_wordsRed.mat'));
+% 
+% cd word2vec/;
+% [dictRed, vecRed] = readWord2Vec('../../data/word2vec/GoogleNews-vectors-negative300.bin', wordsRed, 1);
+% cd ..;
+% 
+% save(strcat('../mat/', fname, '_dictRed.mat'), 'dictRed');
+% save(strcat('../mat/', fname, '_vecRed.mat'), 'vecRed');
+% 
+% M_cos = distMatrix(fname, 'cosine');
+% save('../mat/laptops_distMatrixCos.mat', 'M_cos');
+% M_euc = distMatrix(fname, 'euclidean');
+% save('../mat/laptops_distMatrixEuc.mat', 'M_euc');
 
 % Constructing histograms
-for i = 1:5;
+for i = 3:5;
     constructHist(fname, i);
 end
 
